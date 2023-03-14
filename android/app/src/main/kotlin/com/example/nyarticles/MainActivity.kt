b77b2cf20dbdc81c3ee76a5e9921f94d8a25c22f
Murat Samancı <39999201+mursam@users.noreply.github.com>
2023-03-15 01:11:19 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/nyarticles/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nyarticles
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,199 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const primaryColor = Color.fromARGB(255, 108, 168, 110);
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: primaryColor),
+      home: ArticlesScreen(),
+    );
+  }
+}
+
+class ArticlesScreen extends StatefulWidget {
+  @override
+  _ArticlesScreenState createState() => _ArticlesScreenState();
+}
+
+class _ArticlesScreenState extends State<ArticlesScreen> {
+  List<Article> _articles = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchArticles();
+  }
+
+  void _fetchArticles() async {
+    var response2 = await http.get(Uri.parse(
+        'http://api.nytimes.com/svc/mostpopular/v2/viewed/7.json?api-key=M70JkZFulue0QumafZlYE6jMzRFUkE5j'));
+    final response = response2;
+
+    if (response.statusCode == 200) {
+      final jsonData = json.decode(response.body);
+      setState(() {
+        _articles = (jsonData['results'] as List)
+            .map((item) => Article.fromJson(item))
+            .toList();
+      });
+    } else {
+      print('Error fetching articles: ${response.statusCode}');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: Icon(Icons.menu)),
+        actions: [
+          IconButton(
+              onPressed: () {
+                showSearch(context: context, delegate: Mydelegete());
+              },
+              icon: Icon(Icons.search))
+        ],
+        title: Text(' NY Most Popular Articles'),
+        backgroundColor: primaryColor,
+      ),
+      body: ListView.builder(
+        itemCount: _articles.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            contentPadding: EdgeInsets.symmetric(),
+            leading: Image.network(_articles[index].imageUrl),
+            trailing: Icon(color: primaryColor, Icons.arrow_forward_ios),
+            title: Text(_articles[index].title),
+            subtitle: Text(_articles[index].snippet),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ArticleDetailsScreen(
+                    article: _articles[index],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+class Mydelegete extends SearchDelegate {
+  @override
+  Widget? buildLeading(BuildContext context) => IconButton(
+        onPressed: () => close(context, null),
+        icon: Icon(Icons.arrow_back),
+      );
+
+  @override
+  List<Widget>? buildActions(BuildContext context) => [
+        IconButton(
+            onPressed: () {
+              if (query.isEmpty) {
+                close(context, null);
+              } else {
+                query;
+              }
+            },
+            icon: Icon(Icons.clear))
+      ];
+
+  @override
+  Widget buildResults(BuildContext context) => Center(
+        child: Text(
+          query,
+          style: TextStyle(fontSize: 64),
+        ),
+      );
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestions = ["Oscar", "Silicon Valley"];
+
+    return ListView.builder(
+        itemCount: suggestions.length,
+        itemBuilder: (context, index) {
+          final suggestion = suggestions[index];
+        });
+  }
+}
+
+class ArticleDetailsScreen extends StatelessWidget {
+  final Article article;
+
+  ArticleDetailsScreen({required this.article});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(article.title),
+        backgroundColor: primaryColor,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(article.imageUrl),
+            SizedBox(height: 16),
+            Text(
+              article.title,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 8),
+            Text(
+              article.publishDate,
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Text(
+                article.snippet,
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Article {
+  final String title;
+  final String snippet;
+  final String imageUrl;
+  final String publishDate;
+  final String fullText;
+
+  Article({
+    required this.title,
+    required this.snippet,
+    required this.imageUrl,
+    required this.publishDate,
+    required this.fullText,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    final media = json['media']?.first['media-metadata']?.first;
+    final imageUrl = media != null ? media['url'] : '';
+    final fulltext = json['text'];
+
+    return Article(
+      title: json['title'],
+      snippet: json['abstract'],
+      imageUrl: imageUrl,
+      publishDate: json['published_date'],
+      fullText: json['url'],
+    );
+  }
+}
